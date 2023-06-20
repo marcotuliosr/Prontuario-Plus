9bf80646ab0b04f1c3f0808bebd7ba164b5a8aef
Juliana Lobo <sousalobojuliana@gmail.com>
2023-06-20 19:17:17 -0300

    criação da base do projeto

---
+++ prontuario-plus/src/main/java/com/iftm/api/prontuarioplus/ProntuarioPlusApplication.java
@@ -0,0 +1,13 @@
+package com.iftm.api.prontuarioplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProntuarioPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProntuarioPlusApplication.class, args);
+	}
+
+}
---
+++ prontuario-plus/src/test/java/com/iftm/api/prontuarioplus/ProntuarioPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iftm.api.prontuarioplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProntuarioPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
